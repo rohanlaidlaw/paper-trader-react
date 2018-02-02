0f8e7e8531a3bb33934f79cad7e8f3000f5c3c14
nsv <rohanlaidlaw092@gmail.com>
2018-02-02 18:00:49 +0100

    Add React Component

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('Working thus far.');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render () {
+        return (
+            <div>
+                Hello World ReactJS!!!
+            </div>
+        );
+    }
+}
+
+var app = document.getElementById('app');
+
+ReactDOM.render(<Layout />, app);
