17a00867d400006495936e469533b4e2d6dc9693
nsv <rohanlaidlaw092@gmail.com>
2018-05-07 12:21:43 +0200

    Implement Placeholder Card Button Functionality

--- src/components/component.jsx
+++ src/components/component.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Card from 'material-ui/Card';
 
-class MyAwesomeReactComponent extends React.Component {
+
+class CardButton extends React.Component {
+
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.state = ( {
+            min: 0,
+            max: 10,
+            cardCount:0
+        })
+
+        this.addCard = this.addCard.bind(this);
+        this.addMoreCards = this.addMoreCards.bind(this);
     }
 
-    handleClick() {
-        console.log("Working!")
+    addMoreCards(){
+        var cardCount = this.state.cardCount;
+        this.setState({cardCount: cardCount + 1})
+    }
+
+    addCard() {
+        return(
+            <Card style={{float: 'left', padding: 8, margin: 16, width: 600}}>
+                <h2>Data</h2>
+            </Card>)
+    }
+
+    cards() {
+        var rows = [];
+        var cardCount = this.state.cardCount;
+        for (var i=0; i < cardCount; i++) {
+            rows.push(this.addCard())
+        }
+        return rows
     }
 
     render() {
         return (
-            <RaisedButton label="Add Stock" onClick={this.handleClick}>
-            </RaisedButton>
+            <div style={{width: '50%'}}>
+                <RaisedButton label="Track Stock" onClick={this.addMoreCards}/>
+                {this.cards()}
+            </div>
         );
     }
 }
 
 
-export default MyAwesomeReactComponent;
+export default CardButton;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './components/component.jsx';
+import CardButton from './components/component.jsx';
 
 const App = () => (
     <MuiThemeProvider>
-        <MyAwesomeReactComponent />
+        <CardButton />
     </MuiThemeProvider>
 );
 
