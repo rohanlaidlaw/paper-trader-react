2470b53275ed3221e358b1aae8f0f3f80bb7f3ad
nsv <rohanlaidlaw092@gmail.com>
2018-09-20 15:10:20 +1000

    Open Text Field Dialog on Add Button Click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import AddCard from '../containers/AddCard';
 import CardList from './CardList';
 
 
 const App = () => (
-  <MuiThemeProvider>
-    <div>
-      <AddCard />
-      <CardList />
-    </div>
-  </MuiThemeProvider>
+  <div>
+    <AddCard />
+    <CardList />
+  </div>
 );
 
 export default App;
