73a2a17965bb471ddc2babc7dcc955570e412c4c
nsv <volsci@tuta.io>
2019-01-16 14:52:56 +1000

    Test Redux Reducer Add Card Action

---
+++ tests/reducers.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import reducer from '../src/reducers/reducers.js';
+
+describe('reducer', () => {
+
+    it('processes ADD_TODO', () => {
+        expect(
+            reducer({}, {
+                type: 'ADD_CARD',
+                id: 1,
+                data: '1, 2, 3',
+                ticker: 'acme',
+                percentChange: 5,
+            })
+        ).toEqual(
+            {
+                cards:
+                    [
+                        {
+                            id: 1,
+                            data: '1, 2, 3',
+                            ticker: 'acme',
+                            percentChange: 5,
+                        }
+                    ]
+            }
+        );
+
+    })
+});
