3023d179c297778ed7da9ebfb0f40f7a903b2556
nsv <volsci@tuta.io>
2019-02-01 17:08:52 +1000

    Cypress Test Dialog Box

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,14 +2,32 @@
 
 describe('Main Page', () => {
     beforeEach(function () {
-        cy.visit('http://localhost:3300/')
+        cy.visit('http://localhost:3300/');
     });
 
     it('shows correct app bar title', () => {
-        cy.get('div').contains('Paper Trader')
+        cy.get('div').contains('Paper Trader');
     });
 
     it('shows buttons in app bar', () => {
-        cy.get('button').should('have.length', 2)
+        cy.get('button').should('have.length', 2);
     });
+
+});
+
+describe('Dialog Box', () => {
+    beforeEach(function () {
+        cy.visit('http://localhost:3300/');
+        cy.get('.jss40 > :nth-child(3)').click();
+    });
+
+    it('is spawned correctly', () => {
+        cy.get('.jss100 > .jss13');
+    });
+
+    it('is spawned correctly', () => {
+        cy.get('.jss177 > :nth-child(1)');
+        cy.get('.jss177 > :nth-child(2)');
+    });
+
 });
