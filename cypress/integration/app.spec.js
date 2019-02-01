84b44da5e542d50ff061fee518b4f275327bbe53
nsv <volsci@tuta.io>
2019-02-01 17:30:36 +1000

    Cypress Test Dialog Box Add Button and Test Stock Rendering

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -30,6 +30,20 @@ describe('Dialog Box', () => {
         cy.get('.jss177 > :nth-child(2)');
     });
 
+    it('typing and add button works', () => {
+        cy.get('#name').type('TSLA');
+        cy.get('.jss177 > :nth-child(2)').click();
+        cy.get('.jss100 > .jss13').should('not.exist');
+        cy.get('ul > :nth-child(1)').contains('TSLA');
+
+        cy.get('.jss40 > :nth-child(3)').click();
+        cy.get('#name').type('AAPL');
+        cy.get('.jss266 > :nth-child(2)').click();
+        cy.get('.jss100 > .jss13').should('not.exist');
+        cy.get('ul > :nth-child(2)').contains('AAPL');
+
+    });
+
     it('cancel button works', () => {
         cy.get('.jss177 > :nth-child(1)').click();
         cy.get('.jss100 > .jss13').should('not.exist');
