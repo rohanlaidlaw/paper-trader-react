55ea5d9bd2c9f99277c44554129d17d56c08a197
nsv <volsci@tuta.io>
2019-02-01 17:35:49 +1000

    Cleanup Test Files

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,51 +1,49 @@
 /* eslint-disable */
 
 describe('Main Page', () => {
-    beforeEach(function () {
-        cy.visit('http://localhost:3300/');
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3300/');
+  });
 
-    it('shows correct app bar title', () => {
-        cy.get('div').contains('Paper Trader');
-    });
+  it('shows correct app bar title', () => {
+    cy.get('div').contains('Paper Trader');
+  });
 
-    it('shows buttons in app bar', () => {
-        cy.get('button').should('have.length', 2);
-    });
-
+  it('shows buttons in app bar', () => {
+    cy.get('button').should('have.length', 2);
+  });
 });
 
 describe('Dialog Box', () => {
-    beforeEach(function () {
-        cy.visit('http://localhost:3300/');
-        cy.get('.jss40 > :nth-child(3)').click();
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3300/');
+    cy.get('.jss40 > :nth-child(3)').click();
+  });
 
-    it('is spawned correctly', () => {
-        cy.get('.jss100 > .jss13');
-    });
+  it('is spawned correctly', () => {
+    cy.get('.jss100 > .jss13');
+  });
 
-    it('has correct buttons', () => {
-        cy.get('.jss177 > :nth-child(1)');
-        cy.get('.jss177 > :nth-child(2)');
-    });
+  it('has correct buttons', () => {
+    cy.get('.jss177 > :nth-child(1)');
+    cy.get('.jss177 > :nth-child(2)');
+  });
 
-    it('typing and add button works', () => {
-        cy.get('#name').type('TSLA');
-        cy.get('.jss177 > :nth-child(2)').click();
-        cy.get('.jss100 > .jss13').should('not.exist');
-        cy.get('ul > :nth-child(1)').contains('TSLA');
+  it('typing and add button works', () => {
+    cy.get('#name').type('TSLA');
+    cy.get('.jss177 > :nth-child(2)').click();
+    cy.get('.jss100 > .jss13').should('not.exist');
+    cy.get('ul > :nth-child(1)').contains('TSLA');
 
-        cy.get('.jss40 > :nth-child(3)').click();
-        cy.get('#name').type('AAPL');
-        cy.get('.jss266 > :nth-child(2)').click();
-        cy.get('.jss100 > .jss13').should('not.exist');
-        cy.get('ul > :nth-child(2)').contains('AAPL');
+    cy.get('.jss40 > :nth-child(3)').click();
+    cy.get('#name').type('AAPL');
+    cy.get('.jss266 > :nth-child(2)').click();
+    cy.get('.jss100 > .jss13').should('not.exist');
+    cy.get('ul > :nth-child(2)').contains('AAPL');
+  });
 
-    });
-
-    it('cancel button works', () => {
-        cy.get('.jss177 > :nth-child(1)').click();
-        cy.get('.jss100 > .jss13').should('not.exist');
-    });
+  it('cancel button works', () => {
+    cy.get('.jss177 > :nth-child(1)').click();
+    cy.get('.jss100 > .jss13').should('not.exist');
+  });
 });
